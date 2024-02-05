59b6279fbe7435ef4229bda0ee6cbbc022d29e93
kkyusuftk <yusuf@clevertap.com>
2024-02-05 14:17:25 +0530

    test

--- app/pages/api/hello.js
+++
@@ -1,7 +0,0 @@
-export default async function handler(req, res) {
-  try {
-    res.status(200).json({ hello: "world" })
-  } catch (err) {
-    res.status(500).json({ error: 'failed to load data' })
-  }
-}
